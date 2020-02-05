23e35f236975f724e7b8ab893e418eb8c7a6e28e
Carlos Eduardo Witte <cewitte@outlook.com>
2020-02-05 19:45:54 -0300

    First commit

---
+++ main.go
@@ -0,0 +1,69 @@
+// Command quickstart generates an audio file with the content "Hello, World!".
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	texttospeech "cloud.google.com/go/texttospeech/apiv1"
+	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
+)
+
+func main() {
+	// Instantiates a client.
+	ctx := context.Background()
+
+	client, err := texttospeech.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	maxp := flag.String("ssml", "agvo.ssml", "-ssml=agvo.ssml")
+	flag.Parse()
+
+	file, err := ioutil.ReadFile(*maxp)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fileContents := string(file)
+
+	// Perform the text-to-speech request on the text input with the selected
+	// voice parameters and audio file type.
+	req := texttospeechpb.SynthesizeSpeechRequest{
+		// Set the text input to be synthesized.
+		Input: &texttospeechpb.SynthesisInput{
+			InputSource: &texttospeechpb.SynthesisInput_Ssml{
+				Ssml: fileContents,
+			},
+		},
+		// Build the voice request, select the language code ("en-US") and the SSML
+		// voice gender ("neutral").
+		Voice: &texttospeechpb.VoiceSelectionParams{
+			LanguageCode: "pt-br",
+			Name:         "pt-BR-Wavenet-A",
+			SsmlGender:   texttospeechpb.SsmlVoiceGender_FEMALE,
+		},
+		// Select the type of audio file you want returned.
+		AudioConfig: &texttospeechpb.AudioConfig{
+			AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+		},
+	}
+
+	resp, err := client.SynthesizeSpeech(ctx, &req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// The resp's AudioContent is binary.
+	filename := strings.Trim(*maxp, ".ssml") + ".mp3"
+	err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Audio content written to file: %v\n", filename)
+}
